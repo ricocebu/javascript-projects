abc2a58e73c8449d86da0b3ef580bbc0cfe4682c
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-07-22 09:09:27 +0800

    added autoplay and updates dot when active or selected

--- 06-image-slider/script.js
+++ 06-image-slider/script.js
@@ -5,29 +5,51 @@ const imageArray = [
   "images/cha-hae-in.jpg"
 ];
 
+setInterval(autoPlay, 3000);
+
+
 const container = document.querySelector('#image-container');
+const dots = document.querySelector('#dots').classList;
 let currentIndex = 0;
 document.querySelector('#image-container').src = imageArray[currentIndex]
 
 document.querySelector('.next').addEventListener("click", () => {
   currentIndex = (currentIndex + 1) % imageArray.length;
   container.src = imageArray[currentIndex];
+  updateDots();
 })
-
 document.querySelector('.previous').addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + imageArray.length) % imageArray.length;
   container.src = imageArray[currentIndex];
+  updateDots();
 })
 
 for (let i = 0; i < imageArray.length; i++) {
   const dot = document.createElement("span")
   dot.innerHTML = "\u2022";
   document.querySelector("#dots").appendChild(dot);
+  dot.classList.add('dot-style');
   dot.addEventListener("click", () => {
     currentIndex = i;
     container.src = imageArray[i]
+    updateDots();
   })
 }
 
-const dots = document.querySelector('#dots').classList;
-dots.add('dot-style');
+function updateDots() {
+  const getDots = document.querySelectorAll('span');
+
+  for (let i = 0; i < getDots.length; i++) {
+    getDots[i].classList.remove('dot-style-active');
+    if (i === currentIndex) {
+      getDots[i].classList.add('dot-style-active');
+    }
+  }
+}
+
+function autoPlay() {
+  document.querySelector('.next')
+  currentIndex = (currentIndex + 1) % imageArray.length;
+  container.src = imageArray[currentIndex];
+  updateDots();
+}
