53c9c25ca0bd3cce707cd0dd0a025ce0ad420c05
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-07-24 22:33:33 +0800

    add passwordstrength showing

--- 07-password-strength-checker/script.js
+++ 07-password-strength-checker/script.js
@@ -1,6 +1,7 @@
 const password = document.querySelector('#input-password');
 const passwordResult = document.querySelector('#result-password');
 const passwordShow = document.querySelector('#input-show-password');
+const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
 
 passwordShow.addEventListener('click', () => {
   if (password.type === 'password') {
@@ -12,3 +13,14 @@ passwordShow.addEventListener('click', () => {
   }
 })
 
+password.addEventListener('input', () => {
+  if (password.value.length <= 6 ) {
+    passwordResult.textContent = "Password too weak";
+  }  if (password.value.length > 8 && specialChar.test(password.value) && /[A-Z]/.test(password.value)) {
+    passwordResult.textContent = "Secured";
+  } else if (password.value.length > 6 && specialChar.test(password.value)) {
+    passwordResult.textContent = "Medium";
+  } else if (password.value.length <= 6 ) {
+    passwordResult.textContent = "Password too weak";
+  }  
+})
