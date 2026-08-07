e6ceac1ddb4b272250197fa30d1fbf0fdf37b424
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-07 23:41:59 +0800

    add ids and call them on js.

---
+++ 08-bmi-calculator/script.js
@@ -0,0 +1,5 @@
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const calculate = document.getElementById('calculate');
+const reset = document.getElementById('reset');
+const result = document.getElementById('result');
