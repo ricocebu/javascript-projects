458e04ac71c42460621452aab9aec783bae8f333
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-08 23:21:45 +0800

    Testing event listener.

--- 08-bmi-calculator/script.js
+++ 08-bmi-calculator/script.js
@@ -1,5 +1,18 @@
-const height = document.getElementById('height');
-const weight = document.getElementById('weight');
+let height = document.getElementById('height');
+let weight = document.getElementById('weight');
 const calculate = document.getElementById('calculate');
 const reset = document.getElementById('reset');
 const result = document.getElementById('result');
+
+height.addEventListener('input', () => {
+  height = event.target.value;
+})
+
+weight.addEventListener('input', () => {
+  weight = event.target.value;
+})
+
+calculate.addEventListener('click', () => {
+  console.log(height + weight);
+})
+
