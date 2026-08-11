52acd5f39f1b83d38d64249bb79b9df069287f47
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-11 17:31:00 +0800

    add category.

--- 08-bmi-calculator/script.js
+++ 08-bmi-calculator/script.js
@@ -3,6 +3,7 @@ let weight = document.getElementById('weight');
 const calculate = document.getElementById('calculate');
 const reset = document.getElementById('reset');
 let result = document.getElementById('result');
+let category = document.getElementById('category');
 
 height.addEventListener('input', () => {
   height = event.target.value;
@@ -16,11 +17,21 @@ calculate.addEventListener('click', () => {
   height /= 100;
   let bmiResult = weight / (height * height);
   let n = bmiResult.toFixed(1);
-  result.textContent += n + " kg/m\u00B2";
+  result.textContent = 'Result: ' + n + " kg/m\u00B2";
+
+  if (bmiResult >= 30) {
+    category.textContent = 'Category: Obese';
+  } else if (bmiResult >= 25 && bmiResult <= 29.9) {
+    category.textContent = 'Category: Overweight';
+  } else if (bmiResult >= 18.5 && bmiResult <= 24.9) {
+    category.textContent = 'Category: Normal';
+  } else if (bmiResult < 18.5) {
+    category.textContent = 'Category: Underweight';
+  }
+
 })
 
 reset.addEventListener('click', () => {
   result.textContent = 'Result: ';
+  category.textContent = 'Category: ';
 });
-
-
