5cbeb824758641f4a4f324f60efb5e0095e0f144
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-12 22:22:50 +0800

    Added html and temp js code.

---
+++ 09-Stopwatch/script.js
@@ -0,0 +1,10 @@
+const timer = document.getElementById('timer');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const resetbutton = document.getElementById('reset-button');
+const lapButton = document.getElementById('lap-button');
+const lapRecord = document.getElementById('lap-record');
+
+startButton.addEventListener('click', () => {
+
+})
