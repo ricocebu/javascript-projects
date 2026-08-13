8af239e0480244d62417cb783475f84ab238264b
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-13 23:19:44 +0800

    add function to stop button.

--- 09-Stopwatch/script.js
+++ 09-Stopwatch/script.js
@@ -1,20 +1,20 @@
 const timer = document.getElementById('timer');
-const startButton = document.getElementById('start-button');
-const stopButton = document.getElementById('stop-button');
-const resetbutton = document.getElementById('reset-button');
-const lapButton = document.getElementById('lap-button');
-const lapRecord = document.getElementById('lap-record');
+// const startButton = document.getElementById('start-button');
+// const stopButton = document.getElementById('stop-button');
+// const resetbutton = document.getElementById('reset-button');
+// const lapButton = document.getElementById('lap-button');
+// const lapRecord = document.getElementById('lap-record');
 
 let count = 0;
 let interval;
 
-// startButton.addEventListener('click', () => {
-//   start();
-// })
-
-function start() {
+function startTimer() {
   interval = setInterval(() => {
     count++;
     timer.innerHTML = count + 1;
   }, 10);
+}
+
+function stopTimer() {
+  clearInterval(interval);
 }
