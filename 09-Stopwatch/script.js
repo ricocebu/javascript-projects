82eb0b303609fa747363837e8ba393c52a2cc7bb
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-18 11:47:51 +0800

    added padstart and reset function.

--- 09-Stopwatch/script.js
+++ 09-Stopwatch/script.js
@@ -1,6 +1,6 @@
 const timer = document.getElementById('timer');
-// const startButton = document.getElementById('start-button');
-// const stopButton = document.getElementById('stop-button');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
 // const resetbutton = document.getElementById('reset-button');
 // const lapButton = document.getElementById('lap-button');
 // const lapRecord = document.getElementById('lap-record');
@@ -14,10 +14,18 @@ function startTimer() {
     let ms = count % 100;
     let seconds = Math.floor(count / 100) % 60;
     let minutes = Math.floor(count / 6000);
+    ms = String(ms).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
     timer.innerHTML = minutes + ":" + seconds +":" + ms;
   }, 10);
 }
 
 function stopTimer() {
   clearInterval(interval);
+}
+
+function resetTimer() {
+  count = 0;
+  timer.innerHTML = "00:00:00";
 }
