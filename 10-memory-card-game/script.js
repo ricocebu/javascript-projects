072bc9e9c5220645a44da2860300492dcc90e5fa
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-20 13:45:07 +0800

    Fixed the card selection in the game.

--- 10-memory-card-game/script.js
+++ 10-memory-card-game/script.js
@@ -32,32 +32,66 @@ cardsArray.forEach(url => {
   cardsContainer.appendChild(img);
 
   img.addEventListener('click', () => {
-    selectedCards.push(img.dataset.card);
-    
-    if (img.dataset.hide === "hidden" && selectedCards.length <= 2) {
-      img.dataset.hide = "shown";
-      img.src = img.dataset.card;
 
-      if (selectedCards.length === 2) {
-        img.dataset.hide = "shown";
-        img.src = img.dataset.card;
-
-        if (selectedCards[0] === selectedCards[1]) {
-          img.dataset.hide = "shown";
-          img.src = img.dataset.card;
-        } else {
-          img.dataset.hide = "hidden";
-          img.src = faceDownCard;
-        }
-
-      }
-
-    } else if (img.dataset.hide === "shown") {
-      img.dataset.hide = "hidden";
-      img.src = faceDownCard;
+    if (selectedCards.length >= 2) {
+      return;
     }
 
-    console.log(selectedCards);
+    if (img.dataset.hide === 'show') {
+      return;
+    }
+
+    img.dataset.hide = 'show';
+    img.src = img.dataset.card;
+
+    selectedCards.push(img);
+
+    if (selectedCards.length === 2) {
+
+      const firstCard = selectedCards[0];
+      const secondCard = selectedCards[1];
+
+      if (firstCard.dataset.card === secondCard.dataset.card) {
+        console.log("MATCH!");
+      } else {
+        console.log ("NOT MATCH!");
+
+        firstCard.dataset.hide = 'hidden';
+        firstCard.src = faceDownCard;
+
+        secondCard.dataset.hide = 'hidden';
+        secondCard.src = faceDownCard;        
+      }
+
+      selectedCards.length = 0;
+
+    }
+
+    // selectedCards.push(img.dataset.card);
+    
+    // if (img.dataset.hide === "hidden" && selectedCards.length <= 2) {
+    //   img.dataset.hide = "shown";
+    //   img.src = img.dataset.card;
+
+    //   if (selectedCards.length === 2) {
+
+    //     img.dataset.hide = "shown";
+    //     img.src = img.dataset.card;
+
+    //     if (selectedCards[0] === selectedCards[1]) {
+    //       img.dataset.hide = "shown";
+    //       img.src = img.dataset.card;
+    //     } else {
+    //       img.dataset.hide = "hidden";
+    //       img.src = faceDownCard;
+    //     }
+    //   } 
+
+    // } else if (img.dataset.hide === "shown") {
+    //   img.dataset.hide = "hidden";
+    //   img.src = faceDownCard;
+    // }
+    // console.log(selectedCards);
 
   });
 
