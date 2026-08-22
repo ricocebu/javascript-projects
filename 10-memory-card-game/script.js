ad1cedcfd756c6db16b4f80ea7565e0b5db0e3e0
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-22 23:40:01 +0800

    Update the comments, and explain every code for review.

--- 10-memory-card-game/script.js
+++ 10-memory-card-game/script.js
@@ -1,8 +1,10 @@
+// Calls the html elements and the image.
 const cardsContainer = document.getElementById('cards');
 const faceDownCard = 'images/back_light.png';
 const scoreDisplay = document.getElementById('score');
 const restartGame = document.getElementById('restart');
 
+// Created a arrays of images 8 duplicate images.
 const cardsArray = [
   'images/clubs_A.png',
   'images/clubs_J.png',
@@ -23,16 +25,22 @@ const cardsArray = [
   'images/hearts_K.png'
 ];
 
+// the selected cards array and the score is empty by default. And the checking cards variable is false by default.
 const selectedCards = [];
 let score = 0;
 let checkingCards = false;
 
-
+// This function shuffle the cards array to random when called.
 function shuffleCards() {
   cardsArray.sort(() => Math.random() - 0.5);
 }
 
 
+// This function creates card if called. First it calls the card container and add blank value.
+// Second it creates a foreach loop for the cards array. Inside the foreach loop, it creates a img element.
+// Set the img source to facedown as default, create a card dateset to url when called it will show the actual card.
+// Also the img data set is hide default, and alt image for all cards are "card image", with a 80px width style.
+// And the insert all created img element using append child.
 function createCards() {
   cardsContainer.innerHTML = '';
 
@@ -48,32 +56,33 @@ function createCards() {
 
     cardsContainer.appendChild(img);
 
+    // Event listener for the image once clicked.
     img.addEventListener('click', () => {
 
-      // Don't allow clicks while checking two cards
+      // If the checking cards is true clicking other cards will not be possible.
       if (checkingCards) {
         return;
       }
 
-      // Don't allow more than two selected cards
+      // If more than 2 cards are selected this will prevent the clicking of another card.
       if (selectedCards.length >= 2) {
         return;
       }
 
-      // Don't allow clicking an already revealed card
+      // If the selected card is already clicked. this will prevent the clicking of the same card.
       if (img.dataset.hide === 'show') {
         return;
       }
 
-      // Show card
+      // The value of the card will be show and the card selected will be shown.
       img.dataset.hide = 'show';
       img.src = img.dataset.card;
 
-      // Store the actual image element
+      // The selected cards will be stored on the selected cards array.
       selectedCards.push(img);
 
 
-      // Wait until two cards have been selected
+      // If 2 cards is selected it will be stored on the firstcard and the second card variable. And the checking card will be true.
       if (selectedCards.length === 2) {
 
         const firstCard = selectedCards[0];
@@ -82,7 +91,8 @@ function createCards() {
         checkingCards = true;
 
 
-        // MATCH
+        // If first card and second card is match the score will add 1 and will be shown in the score board.
+        // The selected cards and checking cards will be back to 0 and false like the default set.
         if (firstCard.dataset.card === secondCard.dataset.card) {
 
           score++;
@@ -94,7 +104,7 @@ function createCards() {
           checkingCards = false;
 
 
-          // WIN
+          // If score is equals to 8, there will be a congratulations alert after 300ms.
           if (score === 8) {
             setTimeout(() => {
               alert('You win! Congratulations');
@@ -103,7 +113,8 @@ function createCards() {
 
         }
 
-        // NOT MATCH
+        // If not matched. The value of first and second card will be hidden and will be back to facedowncard.
+        // the selected cards will be back to 0, and the checking cards will be back to false. all of this will run after 500ms.
         else {
 
           setTimeout(() => {
@@ -127,12 +138,13 @@ function createCards() {
 }
 
 
-// START GAME
+// This will shuffle the cards, create cards. like the game started
 shuffleCards();
 createCards();
 
 
-// RESTART GAME
+// If restart game button is clicked the score will be back to 0, the selectedcards array will be 0, the checking cards will be back to false.
+// the scoreboard will be 0, it will create another cards and shuffle again.
 restartGame.addEventListener('click', () => {
 
   score = 0;
